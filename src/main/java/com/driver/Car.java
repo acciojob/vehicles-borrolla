48f4151216fbb609464ea27fddfd89d7f5cfb051
shravan kumar <sravan3251@gmail.com>
2025-01-18 21:49:53 +0530

    vehicle assignment completed with changes made

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,15 +1,26 @@
 package com.driver;
 
 public class Car extends Vehicle {
-    private  int wheels;
+    private final int wheels;
     private  String type;
-    private  int doors;
-    private  int gears;
-    private  boolean isManual;
+    private final int doors;
+    private final int gears;
+    private final boolean isManual;
     private  int currentGear;
-    private  int seats;
+    private final int seats;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats, int wheels1) {
+    private int currentSpeed;
+    private int currentDirection;
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
         super(name);
         this.wheels = wheels;
@@ -20,10 +31,40 @@ public class Car extends Vehicle {
         this.seats = seats;
 
     }
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
 
     public void changeGear(int newGear){
         this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+    }
+
+    public void move(int newSpeed, int newDirection) {
+        this.currentSpeed = newSpeed;
+        this.currentDirection = newDirection;
+        System.out.println("move method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,10 +1,8 @@
 package com.driver;
-
 public class F1 extends Car {
-
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name, 4, 2,6, isManual, "F1", 1, 1);
+        super(name, 4, 2, 6, isManual, "F1", 1);
     }
 
     public void accelerate(int rate){
