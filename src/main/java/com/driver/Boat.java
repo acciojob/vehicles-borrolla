90cd26dc003ee24d9ae2d929bbde154db3aa7f60
shravan kumar <sravan3251@gmail.com>
2025-01-20 15:55:48 +0530

    vehicle assignment completed with change made

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,37 +1,37 @@
 package com.driver;
+
 public class Boat implements WaterVehicle {
-    public String name;
-    public int capacity;
-    public Boat(String name, int capacity) {
+
+        public String name;
+        public int capacity;
+        public Boat(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
+    public void setName(String name)
+    {
         this.name = name;
-        this.capacity = capacity;
     }
     public String getVehicleName()
     {
 
         return name;
     }
-
-    public void setName(String name)
-    {
-        this.name = name;
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
     }
     public int getVehicleCapacity() {
 
         return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
     }
     @Override
     public String toString()
     {
         return "Boat{" + "name='" + name + '\'' + ", capacity=" + capacity + '}';
     }
- public void displayBoatDetails() {
-     System.out.println(this.toString());
+     public void displayBoatDetails() {
+         System.out.println(this.toString());
 
- }
+     }
 }
 //add one more line
