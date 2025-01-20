b2de64308f54a89ab031d4cf40a3cb009685000a
shravan kumar <sravan3251@gmail.com>
2025-01-20 15:48:14 +0530

    vehicle assignment completed with change made

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,33 +1,5 @@
 
 package com.driver.test;
-
-import com.driver.Vehicle;
-import org.junit.Assert;
-import org.junit.Test;
-
 public class TestCases {
 
-    @Test
-    public void testCar1() {
-        Vehicle car = new Vehicle("TestCar");
-        car.steer(45);
-        Assert.assertEquals(45, car.getCurrentDirection());
-    }
-
-    @Test
-    public void testCar2() {
-        Vehicle car = new Vehicle("TestCar");
-        car.move(10, 45);
-        Assert.assertEquals(10, car.getCurrentSpeed());
-        Assert.assertEquals(45, car.getCurrentDirection());
-    }
-
-    @Test
-    public void testF12() {
-        Vehicle car = new Vehicle("TestCar");
-        car.steer(90);
-        car.stop();
-        Assert.assertEquals(0, car.getCurrentSpeed());
-        Assert.assertEquals(90, car.getCurrentDirection());
-    }
 }
