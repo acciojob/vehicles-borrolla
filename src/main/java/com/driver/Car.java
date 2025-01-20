206e7e0adce4e00be6237bb9c7ed0185000fe220
shravan kumar <sravan3251@gmail.com>
2025-01-20 23:41:18 +0530

    vehicle assignment completed with changes made

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,24 +1,14 @@
 package com.driver;
 
 public class Car extends Vehicle {
-    private final int wheels;
+    private  int wheels;
     private  String type;
-    private final int doors;
-    private final int gears;
-    private final boolean isManual;
+    private  int doors;
+    private  int gears;
+    private  boolean isManual;
     private  int currentGear;
-    private final int seats;
+    private int seats;
 
-    private int currentSpeed;
-    private int currentDirection;
-
-    public int getCurrentSpeed() {
-        return currentSpeed;
-    }
-
-    public int getCurrentDirection() {
-        return currentDirection;
-    }
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
@@ -37,18 +27,11 @@ public class Car extends Vehicle {
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
-    public void move(int newSpeed, int newDirection) {
-        this.currentSpeed = newSpeed;
-        this.currentDirection = newDirection;
-        System.out.println("move method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
-    }
-
     public void changeSpeed(int newSpeed, int newDirection){
         // Calls the move method from the Vehicle class
         move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
-
 
     public String getType() {
         return type;
